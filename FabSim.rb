1a133a768a9c70de7ad1094fa6889c224ee77c3a
Alexander "Dondy" Mueller <ddondy@gmail.com>
2010-06-21 17:49:25 +0200

    added shorthands
    improves readability

--- FabSim.rb
+++ FabSim.rb
@@ -5,11 +5,30 @@ f ||= [ # factory map ] {{{
 w ||= [[0,0]] * f.length ** 2
 b ||= [ nil ] * w.length
 t ||= 0 #}}}
+
+class Array #{{{
+  alias has? include?
+
+  def to_dir #{{{
+    x,y = self
+    [[x,y-1],[x-1,y],
+     [x,y+1],[x+1,y]]
+  end #}}}
+
+  def pos?(x,y) #{{{
+    0 <= x and x < self.length and
+    0 <= y and y < self[x].length and
+    self[x][y] != '.'
+  end #}}}
+
+end #}}}
+
 tick = Fiber.new{ # generate ticks on f {{{
   loop{ # to infinity and beyond!
     w.each_index{|i|
       if b[i] # workpiece is buffered {{{
 	e,j = b[i]
+
 	if j > 0
 	  b[i] = [e,j-1]
 	else
@@ -17,11 +36,9 @@ tick = Fiber.new{ # generate ticks on f {{{
 	  w[i] = e
 	end #}}}
       else # workpiece not buffered {{{
-	x,y = w[i]
-	x,y = [[x,y-1],[x-1,y],
-	       [x,y+1],[x+1,y]].find(lambda{w[i]}){|x,y|
-	  0 <= x and x < f.length and 0 <= y and y < f[x].length and
-	  f[x][y] != '.' and not w.include? [x,y] }
+	x,y = w[i].to_dir.find(lambda{w[i]}){|x,y|
+	  f.pos?(x,y) and not w.has? [x,y] }
+
 	case f[x][y]
 	when 'T'; w[i] = [ x, y]
 	when 'E'; w[i] = [ 0, 0]
@@ -29,13 +46,21 @@ tick = Fiber.new{ # generate ticks on f {{{
 	end #}}}
       end }
     Fiber.yield t += 1 } } #}}}
+
 BEGIN{ require 'yaml' # Serialization {{{
   f,w,b,t = YAML.load File.open('FacSim.yaml') if File.exist? 'FacSim.yaml' }
-END{ YAML.dump [f,w,b,t], File.open('FacSim.yaml','w') } #}}}
-loop{ s = '' #TODO: replace CUI with a platform independent GUI {{{
-  f.each_index{|x| s += "\n" 
-    f[x].each_index{|y| s += (w.include?([x,y]) ? '[%s]':' %s ') % f[x][y] }
-    b.each{|v,_| s += '+' if v and v[0] == x } }
+
+END{
+  YAML.dump [f,w,b,t], File.open('FacSim.yaml','w') } #}}}
+
+loop{ #TODO: replace CUI with a platform independent GUI {{{
+  s = ''
+  f.each_index{|x|
+    s += "\n" 
+    f[x].each_index{|y|
+      s += (w.has?([x,y]) ? '[%s]':' %s ') % f[x][y] }
+    b.each{|v,_|
+      s += '+' if v and v[0] == x } }
   print s += "\n#{t}? "
   File.open('FacSim.log','a'){|fh| fh.puts s } # Logging...
   break if 0 >= gets.to_i.times{ tick.resume } } #}}}
