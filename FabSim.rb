2134b509f0fad1a344319d3e1473d223b158489e
Alexander "Dondy" Mueller <ddondy@gmail.com>
2010-06-21 14:50:36 +0200

    added foldmarkers

--- FabSim.rb
+++ FabSim.rb
@@ -5,7 +5,7 @@ f ||= [ # factory map ] {{{
 w ||= [[0,0]] * f.length ** 2
 b ||= [ nil ] * w.length
 t ||= 0 #}}}
-tick = Fiber.new{ # generate ticks on f
+tick = Fiber.new{ # generate ticks on f {{{
   loop{ # to infinity and beyond!
     w.each_index{|i|
       if b[i] # workpiece is buffered {{{
@@ -28,14 +28,14 @@ tick = Fiber.new{ # generate ticks on f
 	when 'B'; w[i] = [-1,-1]; b[i] = [[x,y],i]
 	end #}}}
       end }
-    Fiber.yield t += 1 } }
+    Fiber.yield t += 1 } } #}}}
 BEGIN{ require 'yaml' # Serialization {{{
   f,w,b,t = YAML.load File.open('FacSim.yaml') if File.exist? 'FacSim.yaml' }
 END{ YAML.dump [f,w,b,t], File.open('FacSim.yaml','w') } #}}}
-loop{ s = '' #TODO: replace CUI with a platform independent GUI
+loop{ s = '' #TODO: replace CUI with a platform independent GUI {{{
   f.each_index{|x| s += "\n" 
     f[x].each_index{|y| s += (w.include?([x,y]) ? '[%s]':' %s ') % f[x][y] }
     b.each{|v,_| s += '+' if v and v[0] == x } }
   print s += "\n#{t}? "
   File.open('FacSim.log','a'){|fh| fh.puts s } # Logging...
-  break if 0 >= gets.to_i.times{ tick.resume } }
+  break if 0 >= gets.to_i.times{ tick.resume } } #}}}
